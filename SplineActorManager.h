0ff625f4081491822c5a9264c6cd2ba515d4c207
Kenufu <982531420@qq.com>
2025-05-17 16:35:48 +0800

    SplineActorManager模块引入

---
+++ SplineActorManager.h
@@ -0,0 +1,63 @@
+#ifndef SPLINE_ACTOR_MANAGER_H
+#define SPLINE_ACTOR_MANAGER_H
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <TopoDS_Shape.hxx>
+#include <BRepTools.hxx>
+#include <STEPControl_Reader.hxx>
+#include <vtkRenderWindow.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkActor.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkNew.h>
+#include <vtkRenderer.h>
+#include <iostream>
+#include <IVTKTools_ShapeDataSource.hxx>
+#include <vtkNew.h>
+#include <vtkAssembly.h>
+#include <vtkPropAssembly.h>
+#include <vtkMinimalStandardRandomSequence.h>
+#include <vtkNamedColors.h>
+#include "Core.h"
+#include "ModelActor.h"
+#include "SplineActor.h"
+#include <vtkMultiBlockDataSet.h>
+#include <vtkCompositePolyDataMapper.h>
+#include <iostream>
+#include <vtkRenderWindow.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkActor.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkNew.h>
+#include <vtkRenderer.h>
+#include <iostream>
+#include <QtQml/QQmlApplicationEngine>
+#include <QtQml/QQmlContext>
+#include <QtQml/qqmlregistration.h>
+#include <vtkSphereSource.h>
+#include <vtkOBJReader.h>
+#include <QQuickVTKItem.h>
+#include <QVTKRenderWindowAdapter.h>
+
+class SplineActorManager 
+{
+public:
+	const SplineActor* getSplineActor(Index model_id);
+	void deleteModel(Index model_id);
+	void loadSpline(Index model_id, SplineDataVtk spline_data);
+
+	void setVisibility(bool visibility);
+	Q_INVOKABLE void setRenderMode(Index model_id, SplineRenderMode render_mode);
+
+private:
+	std::unordered_map <Index, std::unique_ptr<SplineActor>> models_;
+	bool edge_render_;
+	SplineRenderMode render_mode_;
+	vtkRenderer* renderer_;
+};
+
+
+#endif
