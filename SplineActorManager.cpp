56f50e11d192f9f7683e9a7297177c6741196aca
Kenufu <982531420@qq.com>
2025-05-17 16:36:09 +0800

    SplineActorManager模块实现

---
+++ SplineActorManager.cpp
@@ -0,0 +1,72 @@
+#include "SplineActorManager.h"
+#include "SplineActorManager.h"
+#include "SplineActorManager.h"
+#include "SplineActorManager.h"
+#include "SplineActorManager.h"
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <TopoDS_Shape.hxx>
+#include <BRepTools.hxx>
+#include <STEPControl_Reader.hxx>
+#include <vtkRenderWindow.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkActor.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkNew.h>
+#include <vtkRenderer.h>
+#include <iostream>
+#include <IVTKTools_ShapeDataSource.hxx>
+#include <vtkNew.h>
+#include <vtkAssembly.h>
+#include <vtkPropAssembly.h>
+#include <vtkMinimalStandardRandomSequence.h>
+#include <vtkNamedColors.h>
+#include "Core.h"
+#include "ModelActor.h"
+#include "SplineActor.h"
+#include <vtkMultiBlockDataSet.h>
+#include <vtkCompositePolyDataMapper.h>
+#include <iostream>
+#include <vtkRenderWindow.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkActor.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkNew.h>
+#include <vtkRenderer.h>
+#include <iostream>
+#include <QtQml/QQmlApplicationEngine>
+#include <QtQml/QQmlContext>
+#include <QtQml/qqmlregistration.h>
+#include <vtkSphereSource.h>
+#include <vtkOBJReader.h>
+#include <QQuickVTKItem.h>
+#include <QVTKRenderWindowAdapter.h>
+
+const SplineActor* SplineActorManager::getSplineActor(Index model_id)
+{
+	return this->models_.at[model_id];
+}
+
+void SplineActorManager::deleteModel(Index model_id)
+{
+	this->models_.erase(model_id);
+}
+
+void SplineActorManager::loadSpline(Index model_id, SplineDataVtk spline_data)
+{
+
+}
+
+void SplineActorManager::setVisibility(bool visibility)
+{
+	this->models_[model_id]->setVisibility(visibility);
+}
+
+Q_INVOKABLE void SplineActorManager::setRenderMode(Index model_id, SplineRenderMode render_mode)
+{
+	this->models_[model_id]->setRenderMode(render_mode);
+	
+}
