87d0058a18b1a5323070c6d65010d8e77ee198d3
a-ciue <htxz_6a6@163.com>
2025-11-09 23:46:02 +0800

    feat(test, build): 允许本地构建的测试程序，无需手动配置PATH环境变量即可在ctest里进行调试

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -3,7 +3,18 @@ function(precess_add_test TARGET)
     if(BUILD_TESTING)
         add_executable(${ARGV})
         target_link_libraries(${TARGET} PRIVATE Catch2::Catch2WithMain)
-        catch_discover_tests(${TARGET})
+
+        set(test_properties)
+        if(WIN32)
+            list(APPEND test_properties
+                # 反斜杠多加几个是为了在生成的.cmake文件中能正确解析，因为在最后对生成器表达式求值时需要一个反斜杠给分号转义，中间有各种转义处理会丢失反斜杠
+			    ENVIRONMENT_MODIFICATION "$<JOIN:$<LIST:TRANSFORM,$<TARGET_RUNTIME_DLL_DIRS:${TARGET}>,PREPEND,PATH=path_list_prepend:>,\\\\\\\\\\\\\\\\\;>"
+            )            
+        endif()
+
+        catch_discover_tests(${TARGET}
+            PROPERTIES ${test_properties}
+        )
     endif()
 endfunction()
 
