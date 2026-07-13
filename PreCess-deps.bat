8359228a12ac17701e7777f8962f32485dc60de3
Young <2109190674@qq.com>
2026-07-13 18:14:01 +0800

    fix(Precess-deps.bat):改为使用gmsh的master分支的occ-8提交，一适应occ8.0的接口

--- PreCess-deps.bat
+++ PreCess-deps.bat
@@ -94,7 +94,10 @@ git clone --single-branch --depth 1 --branch V8_0_0 https://github.com/Open-Casc
 git clone --single-branch --depth 1 --branch v1.16.0 https://github.com/gabime/spdlog.git
 git clone --single-branch --depth 1 --branch v3.11.0 https://github.com/catchorg/Catch2.git
 git clone --single-branch --depth 1 --branch v7.80 https://github.com/LoicMarechal/libMeshb.git
-git clone --single-branch --depth 1 --branch gmsh_4_15_0 https://gitlab.onelab.info/gmsh/gmsh.git
+git clone --single-branch --branch master https://gitlab.onelab.info/gmsh/gmsh.git gmsh-occ8
+pushd gmsh-occ8
+git checkout --detach 86596d7902a1b00e23641ac5c904b7c1f880ce9f
+popd
 curl -L -o OCCT/3rdparty-vc14-64-temp.zip --connect-timeout 30 https://github.com/Open-Cascade-SAS/OCCT/releases/download/V8_0_0/3rdparty-vc14-64.zip
 
 if not defined qtPath (
@@ -156,10 +159,10 @@ if "!buildRelInfo!"=="1" cmake --build . --target install --config RelWithDebInf
 cmake --build . --target install --config Debug
 if "!buildRelease!"=="1" cmake --build . --target install --config Release
 
-REM Clone and build Gmsh 4.15.0
+REM Clone and build Gmsh OCC8
 set "CASROOT=%depsPath%\OpenCASCADE8.0.0"
-pushd "%sourcePath%/gmsh"
-cmake -S . -B ./build "-GNinja Multi-Config" "-DCMAKE_CONFIGURATION_TYPES:STRING=Debug;Release;RelWithDebInfo" "-DCMAKE_INSTALL_PREFIX:PATH=%depsPath%\gmsh4.15.0" -DENABLE_OPENMP:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DENABLE_BUILD_DYNAMIC:BOOL=OFF -DENABLE_BUILD_LIB:BOOL=ON -DENABLE_BUILD_SHARED:BOOL=ON -DCMAKE_INSTALL_MESSAGE=LAZY
+pushd "%sourcePath%/gmsh-occ8"
+cmake -S . -B ./build "-GNinja Multi-Config" "-DCMAKE_CONFIGURATION_TYPES:STRING=Debug;Release;RelWithDebInfo" "-DCMAKE_INSTALL_PREFIX:PATH=%depsPath%\gmsh-occ8" -DENABLE_OCC:BOOL=ON -DENABLE_OPENMP:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DENABLE_BUILD_DYNAMIC:BOOL=OFF -DENABLE_BUILD_LIB:BOOL=ON -DENABLE_BUILD_SHARED:BOOL=ON -DCMAKE_INSTALL_MESSAGE=LAZY
 pushd build
 if "!buildRelInfo!"=="1" cmake --build . --target install --config RelWithDebInfo
 cmake --build . --target install --config Debug
