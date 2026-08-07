a700fd3702300442ca391690cc932adb0f18a494
a-ciue <htxz_6a6@163.com>
2026-08-07 23:14:16 +0800

    refact(edit): 取消edit必须选中component

--- plugins/edit/CreateFacePlugin/CreateFaceHandler.h
+++ plugins/edit/CreateFacePlugin/CreateFaceHandler.h
@@ -6,12 +6,13 @@
 namespace systems::edit {
 /**
  * @brief 创建面的模型编辑处理器Handler，代码实现了创建面的功能
+ * @note 目标组件由选择集的全局点 id 经 pointIdMap 反查决定，不依赖对象树选中组件
  */
 class CreateFaceHandler : public EditHandler {
 public:
     CreateFaceHandler() = default;
     ~CreateFaceHandler() override = default;
-    std::any execute(ComponentOperator& op, const std::vector<core::ArgObject>& args) override;
+    std::any execute(ModelLayer& model, Index fallback_component_id, const std::vector<core::ArgObject>& args) override;
     std::vector<core::ArgType> args_type() const override;
 };
 }
--- plugins/edit/DeleteFacePlugin/DeleteFaceHandler.h
+++ plugins/edit/DeleteFacePlugin/DeleteFaceHandler.h
@@ -6,12 +6,14 @@
 namespace systems::edit {
 /**
  * @brief 删除面的模型编辑处理器Handler，代码实现了删除面的功能
+ * @note 目标组件由选择集携带的 component_id 决定（面 id 为组件内局部 id），
+ *       选择未携带组件身份时以接口提示的 fallback_component_id 兜底
  */
 class DeleteFaceHandler : public EditHandler {
 public:
     DeleteFaceHandler() = default;
     ~DeleteFaceHandler() override = default;
-    std::any execute(ComponentOperator& op, const std::vector<core::ArgObject>& args) override;
+    std::any execute(ModelLayer& model, Index fallback_component_id, const std::vector<core::ArgObject>& args) override;
     std::vector<core::ArgType> args_type() const override;
 };
 }
