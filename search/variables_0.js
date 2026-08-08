c43ca8df69fa9a4f9bb387ce7ac2925df2383d12
a-ciue <77373282+a-ciue@users.noreply.github.com>
2026-08-08 02:52:57 +0000

    Deploying to gh-pages from @ a-ciue/PreCess@03ba0914d1ee5414748b97d740ceb21cbd09037a 🚀

--- search/variables_0.js
+++ search/variables_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['_5fcamera_0',['_camera',['../structQRenderWindow.html#a76af644d33dc5c1d094558d83a83c049',1,'QRenderWindow']]],
-  ['_5fclick_1',['_click',['../structQRenderWindow.html#a51af1a11dab47d58481f2d9789ddba70',1,'QRenderWindow']]]
+  ['_5fclick_0',['_click',['../structQRenderWindow.html#a51af1a11dab47d58481f2d9789ddba70',1,'QRenderWindow']]]
 ];
