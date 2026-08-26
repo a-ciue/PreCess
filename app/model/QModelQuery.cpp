ca77e95d2c43c93c369347f02dd7ac3002724b58
Young <2109190674@qq.com>
2026-08-26 14:55:03 +0800

    fix(renderStrategy):删除无用属性

--- app/model/QModelQuery.cpp
+++ app/model/QModelQuery.cpp
@@ -45,7 +45,6 @@ void appendAttributeInfo(
         item["type"] = static_cast<int>(type);
         item["typeName"] = type_name;
         item["attrType"] = attr_type;
-        item["renderable"] = true;
         item["componentCount"] = component_count;
         out.append(item);
     }
