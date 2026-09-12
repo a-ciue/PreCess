48b23d16d0d080e8e5cceeef7c359d083f21c76c
Chen <201436728@qq.com>
2026-09-12 04:41:12 +0000

    !118 feat: 适配linux/macos编译
    * feat: 适配linux/macos编译

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -13,7 +13,26 @@ function(precess_add_test TARGET)
             list(APPEND test_properties
                 # 反斜杠多加几个是为了在生成的.cmake文件中能正确解析，因为在最后对生成器表达式求值时需要一个反斜杠给分号转义，中间有各种转义处理会丢失反斜杠
 			    ENVIRONMENT_MODIFICATION "$<JOIN:$<LIST:TRANSFORM,${dl_paths},PREPEND,PATH=path_list_prepend:>,\\\\\\\\\\\\\\\\\;>"
-            )            
+            )
+        elseif(UNIX)
+            # Linux/macOS 下依赖库经 rpath 解析：CMAKE_PREFIX_PATH 的各包目录下取 lib，
+            # 保证 VTK/OCCT/Qt 等非系统安装路径下的动态库能被 ctest 找到
+            set(test_build_rpath "")
+            foreach(_prefix IN LISTS CMAKE_PREFIX_PATH)
+                if(EXISTS "${_prefix}/lib")
+                    list(APPEND test_build_rpath "${_prefix}/lib")
+                endif()
+            endforeach()
+            # $ORIGIN 与 @loader_path 语义相同：分别面向 Linux 与 macOS 的动态库加载器
+            if(APPLE)
+                set(test_install_rpath "@loader_path")
+            else()
+                set(test_install_rpath "$ORIGIN")
+            endif()
+            set_target_properties(${TARGET} PROPERTIES
+                BUILD_RPATH "${test_build_rpath}"
+                INSTALL_RPATH "${test_install_rpath}"
+            )
         endif()
 
         catch_discover_tests(${TARGET}
